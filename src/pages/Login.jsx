8579b01e19aeb18e9fbad550e7dcea400f5bb29a
Md. Masum Musfique <musfiquemasum8@gmail.com>
2025-07-31 11:11:54 +0600

    login page updated

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,5 +1,5 @@
 import { useForm } from 'react-hook-form'
-import { useNavigate } from 'react-router'
+import { NavLink, useNavigate } from 'react-router'
 import toast from 'react-hot-toast'
 import { useAuth } from '../hooks/useAuth';
 
@@ -16,19 +16,27 @@ const Login = () => {
       toast.success('Login successful!')
       navigate('/')
     } catch (err) {
-    console.error('Login error:', err)
+      console.error('Login error:', err)
       toast.error('Login failed. Check credentials.')
     }
   }
 
   return (
-    <div className="flex justify-center items-center h-screen">
+    <div className="flex justify-center items-center h-[70vh]">
       <form onSubmit={handleSubmit(onSubmit)} className=" p-6 rounded shadow-2xl w-96">
         <h2 className="text-4xl font-bold mb-4 text-center">Login</h2>
+        <label className="label">Email</label>
         <input type="email" placeholder="Email" {...register('email')} className="input mb-2 w-full" />
+        <label className="label">Password</label>
         <input type="password" placeholder="Password" {...register('password')} className="input mb-2 w-full" />
-        <button type="submit" className="btn w-full bg-blue-600 text-white">Login</button>
+        <button type="submit" className="btn w-full bg-blue-600 text-white mt-5">Login</button>
+
+        
+          <p className='text-center mt-5'>Don't have an Account? <span className='text-blue-500'><NavLink to='/register'>Register</NavLink></span></p>
+      
       </form>
+
+
     </div>
   )
 };
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,6 +1,6 @@
 import { useForm } from 'react-hook-form'
 //import { useAuth } from '../context/AuthContext'
-import { useNavigate } from 'react-router'
+import { NavLink, useNavigate } from 'react-router'
 import toast from 'react-hot-toast'
 import { useAuth } from '../hooks/useAuth'
 
@@ -20,19 +20,31 @@ const Register = () => {
   }
 
   return (
-    <div className="flex justify-center items-center h-screen">
-      <form onSubmit={handleSubmit(onSubmit)} className=" p-6 rounded shadow-md w-96">
+    <div className="flex justify-center items-center h-[70vh]">
+      <form onSubmit={handleSubmit(onSubmit)} className=" p-6 rounded shadow-2xl w-96">
         <h2 className="text-4xl font-bold mb-4 text-center">Register</h2>
+
+        <label className="label">Name</label>
         <input {...regForm('name')} placeholder="Name" className="input mb-2 w-full" />
+
+      <label className="label">Email</label>
         <input {...regForm('email')} placeholder="Email" className="input mb-2 w-full" />
+
+        <label className="label">Password</label>
         <input {...regForm('password')} placeholder="Password" type="password" className="input mb-2 w-full" />
+
+        <label className="label">Phone</label>
         <input {...regForm('phone')} placeholder="Phone" className="input mb-2 w-full" />
+
+        <label className="label">Gender</label>
         <select {...regForm('gender')} className="input mb-2 w-full">
           <option value="">Select Gender</option>
           <option>Male</option>
           <option>Female</option>
         </select>
+
         <button type="submit" className="btn w-full bg-green-600 text-white">Register</button>
+        <p className='text-center mt-5'>Already have an Account? <span className='text-blue-500'><NavLink to='/login'>Login</NavLink></span></p>
       </form>
     </div>
   )
